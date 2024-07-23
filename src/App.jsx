f3e67db8973ad862727dff5d18707374d3a30e9c
Nasir-Technical <nasir.18.2006@gmail.com>
2024-07-23 17:51:49 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Body from "./components/Body";
 import { Toaster } from 'react-hot-toast'; 
 import MovieDialog from "./components/MovieDialog";
+import './index.css';
  
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Ensure paths to your files are correct
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
