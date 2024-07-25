300885fa790c72fc07e0424776319f7503c1b95f
Nasir-Technical <nasir.18.2006@gmail.com>
2024-07-26 01:19:38 +0500

    changing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'; // import PersistGate
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { store, persistor } from './redux/store'; // store aur persistor ko import karein
+import { store, persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,18 +1,16 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import storage from "redux-persist/lib/storage";
 import userReducer from "./userSlice";
 import movieReducer from "./movieSlice";
 import searchSlice from "./searchSlice";
 
-// Combine all reducers
 const rootReducer = combineReducers({
   app: userReducer,
   movie: movieReducer,
   searchMovie: searchSlice
 });
 
-// Persist configuration
 const persistConfig = {
   key: 'root',
   storage,
